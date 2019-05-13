cdfe21d11e2c7d2e983b5341016aa943484610d4
Junjie Chen <junjie.chen18@gmail.com>
2019-05-13 18:45:17 -0400

    feat(src): add more comment JSX

--- src/index.js
+++ src/index.js
@@ -17,6 +17,30 @@ const App = () => {
           <div className="text">Nice blog post!</div>
         </div>
       </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">Alex</a>
+          <div className="metadata">
+            <span className="date">Today at 2:00AM</span>
+          </div>
+          <div className="text">I like the subject!</div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">Jane</a>
+          <div className="metadata">
+            <span className="date">Yesterday at 5:00PM</span>
+          </div>
+          <div className="text">I like the writing!</div>
+        </div>
+      </div>
     </div>
   );
 };
