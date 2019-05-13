1e8ad8427cab6e884c9d6894da1b81f1cb4728e9
Junjie Chen <junjie.chen18@gmail.com>
2019-05-13 19:09:26 -0400

    feat(src): nest CommentDetail component inside of App component

--- src/index.js
+++ src/index.js
@@ -1,46 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">Sam</a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">Alex</a>
-          <div className="metadata">
-            <span className="date">Today at 2:00AM</span>
-          </div>
-          <div className="text">I like the subject!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">Jane</a>
-          <div className="metadata">
-            <span className="date">Yesterday at 5:00PM</span>
-          </div>
-          <div className="text">I like the writing!</div>
-        </div>
-      </div>
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
     </div>
   );
 };
