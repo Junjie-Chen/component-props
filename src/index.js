3cbb066ee0446d4d5c14b45ad14c8a2ba50f012d
Junjie Chen <junjie.chen18@gmail.com>
2019-05-13 20:04:04 -0400

    feat(src): add and use avatar prop

--- src/index.js
+++ src/index.js
@@ -10,16 +10,19 @@ const App = () => {
         author="Sam"
         timeAgo="Today at 4:45PM"
         content="Nice blog post"
+        avatar={faker.image.avatar()}
       />
       <CommentDetail
         author="Alex"
         timeAgo="Today at 2:00AM"
         content="I like the subject"
+        avatar={faker.image.avatar()}
       />
       <CommentDetail
         author="Jane"
         timeAgo="Yesterday at 5:00PM"
         content="I like the writing"
+        avatar={faker.image.avatar()}
       />
     </div>
   );
